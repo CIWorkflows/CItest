8324b98cde8b673461f321140274c106a32d4a07
Joseph Ardizzone <Joseph.V.Ardizzone@nasa.gov>
2022-07-21 10:24:32 -0400

    Building repo

---
+++ task.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import yaml
+
+with open('task.yaml', 'r') as ymlfile:
+    config = yaml.load(ymlfile,Loader=yaml.SafeLoader)
+
+for task in config['tasks']:
+
+    print('Task Name  : ', task.get('name', 'None'))
+    print('Description: ', task.get('description', 'None'))
+    print('Status     : ', task.get('status', 0))
+
+    status = task.get('status', 0)
+    if status < 0:
+        sys.exit(status)
+
+sys.exit(0)
